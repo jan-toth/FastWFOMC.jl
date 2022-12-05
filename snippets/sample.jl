ebd86cad5fb8cb326aaf4ab2d64b07e4310da9e0
Jan Toth <tothjan2@fel.cvut.cz>
2022-12-05 10:04:38 +0100

    Update sample.jl

--- snippets/sample.jl
+++ snippets/sample.jl
@@ -1,19 +1,16 @@
-using Pkg
+# using Pkg
 
-# Use current folder as a Julia project
-Pkg.activate(".")
+# # Use current folder as a Julia project
+# Pkg.activate(".")
 
-# Add FastWFOMC.jl dependency to the current Julia project
-Pkg.add(PackageSpec(url="https://github.com/jan-toth/FastWFOMC.jl"))
+# # Add FastWFOMC.jl dependency to the current Julia project
+# Pkg.add(PackageSpec(url="https://github.com/jan-toth/FastWFOMC.jl"))
 
-# Install all dependencies that might be missing
-Pkg.instantiate()
-
-
-
-
+# # Install all dependencies that might be missing
+# Pkg.instantiate()
 
 using FastWFOMC
 
-cell_graph = get_cell_graph("(~B0(x, y) | S0(x))")
+sentence = ARGS[1]
+cell_graph = get_cell_graph(sentence)
 println(cell_graph)
